c716f9a5b3e729388554cc89e34a4b6bedd27544
SanketZ18 <sanketzagade0018@gmail.com>
2026-02-01 18:35:54 +0530

    new updates

--- backend/src/main/java/com/ayurkisan/AyurkisanApplication.java
+++ backend/src/main/java/com/ayurkisan/AyurkisanApplication.java
@@ -1,1 +1,12 @@
-package com.ayurkisan; public class AyurkisanApplication { public static void main(String[] args) {} }
+package com.ayurkisan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AyurkisanApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AyurkisanApplication.class, args);
+    }
+}
